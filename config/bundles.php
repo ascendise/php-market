0346c974a65515b6042594f6bebb507320cf6f43
Ascendise <ascendise@ascendise.ch>
2025-11-03 00:35:02 +0000

    feat: styling (#3)
    Added a green/greying color scheme and a gamey font.
    
    Reviewed-on: https://gitea.ascendise.ch/ascendise/php-market/pulls/3
    Co-authored-by: Ascendise <ascendise@ascendise.ch>
    Co-committed-by: Ascendise <ascendise@ascendise.ch>

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfonycasts\SassBundle\SymfonycastsSassBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
